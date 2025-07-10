161d7ee3caa3ea421cc01dd334de1060c91e0aa1
eMann <papa16annan@gmail.com>
2025-07-10 10:11:34 +0000

    cpanel git

--- keys.php
+++ keys.php
@@ -1,5 +1,11 @@
 <?php
+//LOCAL
 $edbn = "brightstart";
 $edbuser = "root";
 $epassword = "";
+/*
+//CPANEL
+$edbn = "stcciju4_brightstart";
+$edbuser = "stcciju4_eMann";
+$epassword = "ABCDrugHIJKlsd";*/
 ?>
